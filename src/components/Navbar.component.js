4b219616eb1a02dbeb35da307cdaef38bd824f55
marajul-enosis <marajul.islam@enosisbd.com>
2023-06-24 16:10:22 +0600

    initial commit

---
+++ src/components/Navbar.component.js
@@ -0,0 +1,11 @@
+
+function Navbar(){
+    return(
+        <nav className="navbar bg-primary" data-bs-theme="dark">
+            <h5 className="navbar-brand mx-2">TodoApp</h5>
+        </nav>
+    );
+}
+
+
+export default Navbar;
